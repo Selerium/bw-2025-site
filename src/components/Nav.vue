b9880120ce6f1047e8de10bae3b817ba0865b1ac
Selerium <johnadithya008@gmail.com>
2025-07-01 00:16:54 +0400

    finished first two sections

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -9,17 +9,17 @@ function changeSection(value: number) {
 </script>
  
 <template>
-    <div class="py-4 flex justify-between items-center">
+    <div class="py-4 flex justify-between items-center mix-blend-difference">
         <a href="#" class="transition-all" @click="changeSection(0)">
-            <img class="mix-blend-difference" src="/bw-25-logo.png" />
+            <img src="/bw-25-logo.png" />
         </a>
         <div class="w-1/3 flex justify-between items-center">
-            <a href="#" class="transition-all mix-blend-difference" @click="changeSection(1)" :class="chosenSection == 1 ? 'font-bold text-primary' : 'font-normal text-white'">about</a>
-            <a href="#" class="transition-all mix-blend-difference" @click="changeSection(2)" :class="chosenSection == 2 ? 'font-bold text-primary' : 'font-normal text-white'">theme</a>
-            <a href="#" class="transition-all mix-blend-difference" @click="changeSection(3)" :class="chosenSection == 3 ? 'font-bold text-primary' : 'font-normal text-white'">speakers</a>
-            <a href="#" class="transition-all mix-blend-difference" @click="changeSection(4)" :class="chosenSection == 4 ? 'font-bold text-primary' : 'font-normal text-white'">sign up</a>
+            <a href="#about" class="transition-all text-white" @click="changeSection(1)" :class="chosenSection == 1 ? 'font-bold' : 'font-normal'">about</a>
+            <a href="#theme" class="transition-all text-white" @click="changeSection(2)" :class="chosenSection == 2 ? 'font-bold' : 'font-normal'">theme</a>
+            <a href="#speakers" class="transition-all text-white" @click="changeSection(3)" :class="chosenSection == 3 ? 'font-bold' : 'font-normal'">speakers</a>
+            <a href="#signup" class="transition-all text-white" @click="changeSection(4)" :class="chosenSection == 4 ? 'font-bold' : 'font-normal'">sign up</a>
         </div>
-        <div class="flex gap-10 items-center mix-blend-difference">
+        <div class="flex gap-10 items-center">
             <a href="/"><img class="h-8 w-8" src="/mail.png"/></a>
             <a href="/"><img class="h-8 w-8 ml-1.5" src="/whatsapp.png"/></a>
             <a href="/"><img class="h-8 w-8" src="/instagram.png"/></a>
