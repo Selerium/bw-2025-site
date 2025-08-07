5c5f0c851095c5eb70e20eb4967b022cc3b8e904
John Adithya <jadithya@42student.abudhabi.ae>
2025-08-07 11:39:51 +0530

    fixed build error

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import { onMounted } from 'vue';
-import Home from './components/Home.vue';
 import { supabase } from './lib/supabaseClient';
 
 async function testSupabase() {
