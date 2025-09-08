961a022a97319025e0738d2c7b4862e7a3e3bec5
John Adithya <jadithya@42student.abudhabi.ae>
2025-09-08 15:14:29 +0400

    changed supabase client ocation

--- api/public.js
+++ api/public.js
@@ -1,4 +1,9 @@
-import supabase from '../src/lib/supabaseClient';
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+
+const supabase = createClient(supabaseUrl, supabaseAnonKey)
 
 export default async function handler(req, res) {
   if (req.method === "POST") {
