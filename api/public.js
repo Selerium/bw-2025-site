f19e67869e36599c4234a4e71858f79771e43081
John Adithya <jadithya@42student.abudhabi.ae>
2025-09-08 14:38:02 +0400

    another test

--- api/public.js
+++ api/public.js
@@ -1,27 +1,31 @@
 export default async function handler(req, res) {
-//   if (req.method === "POST") {
-//     const supabaseData = req.body;
-
-//     const supabaseRes = await fetch(
-//       "https://afqmzmqsvbxdyxuqhwfv.supabase.co/functions/v1/register-user",
-//       {
-//         method: "POST",
-//         headers: {
-//           "Content-Type": "application/json",
-//         },
-//         body: supabaseData,
-//       }
-//     );
-
-//     let userResponse = await supabaseRes.json();
-//   } else if (req.method === "GET") {
-//   } else {
-//   }
-  return res
-    .status(405)
-    .json({
-      error: true,
-      title: "Invalid API call.",
-      message: "Something went wrong.",
+  if (req.method === "POST") {
+    const supabaseData = req.body;
+    return res.status(200).json({
+        error: false,
+        title: "Test",
+        message: supabaseData
     });
+
+    // const supabaseRes = await fetch(
+    //   "https://afqmzmqsvbxdyxuqhwfv.supabase.co/functions/v1/register-user",
+    //   {
+    //     method: "POST",
+    //     headers: {
+    //       "Content-Type": "application/json",
+    //     },
+    //     body: supabaseData,
+    //   }
+    // );
+    // let userResponse = await supabaseRes.json();
+
+  }
+  //  else if (req.method === "GET") {
+  //   } else {
+  //   }
+  return res.status(405).json({
+    error: true,
+    title: "Invalid API call.",
+    message: "Something went wrong.",
+  });
 }
