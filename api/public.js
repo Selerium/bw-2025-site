9bfe554a24aee2f477777f81ad105dca68022bcc
John Adithya <jadithya@42student.abudhabi.ae>
2025-09-08 14:40:15 +0400

    even more testing

--- api/public.js
+++ api/public.js
@@ -1,23 +1,23 @@
 export default async function handler(req, res) {
   if (req.method === "POST") {
     const supabaseData = req.body;
+    
+    const supabaseRes = await fetch(
+      "https://afqmzmqsvbxdyxuqhwfv.supabase.co/functions/v1/register-user",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: supabaseData,
+      }
+    );
+    let userResponse = await supabaseRes.json();
     return res.status(200).json({
         error: false,
         title: "Test",
-        message: supabaseData
+        message: userResponse
     });
-
-    // const supabaseRes = await fetch(
-    //   "https://afqmzmqsvbxdyxuqhwfv.supabase.co/functions/v1/register-user",
-    //   {
-    //     method: "POST",
-    //     headers: {
-    //       "Content-Type": "application/json",
-    //     },
-    //     body: supabaseData,
-    //   }
-    // );
-    // let userResponse = await supabaseRes.json();
 
   }
   //  else if (req.method === "GET") {
