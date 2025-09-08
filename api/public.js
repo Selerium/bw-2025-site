87102c113ed4fba4a70505e69b34da106670c6d3
John Adithya <jadithya@42student.abudhabi.ae>
2025-09-08 16:05:50 +0400

    test more test

--- api/public.js
+++ api/public.js
@@ -18,6 +18,14 @@ export default async function handler(req, res) {
             'data': error
         })
     }
+    else {
+        return res.status(200).json({
+            'error': true,
+            'title': 'User registered!',
+            'message': 'Your details have been saved.',
+            'data': error
+        })
+    }
 }
 
 
