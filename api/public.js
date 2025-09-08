63dbec3011f281be5ec26f7bd4be1af83e6f185e
John Adithya <jadithya@42student.abudhabi.ae>
2025-09-08 15:11:20 +0400

    transferring edge function to vercel

--- api/public.js
+++ api/public.js
@@ -1,19 +1,32 @@
+import supabase from '../src/lib/supabaseClient';
+
 export default async function handler(req, res) {
   if (req.method === "POST") {
-    const supabaseData = req.body;
+    const userData = req.body;
     
-    const supabaseRes = await fetch(
-      "https://afqmzmqsvbxdyxuqhwfv.supabase.co/functions/v1/register-user",
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: supabaseData,
-      }
-    );
-    return supabaseRes;
-  }
+    const { data, error } = await supabase.from('registrations').insert(userData);
+    if (error) {
+        return res.status(400).json({
+            'error': true,
+            'title': 'User not registered',
+            'message': 'We were not able to process your request. Please try again later.',
+            'data': error
+        })
+    }
+}
+
+
+// const supabaseRes = await fetch(
+//   "https://afqmzmqsvbxdyxuqhwfv.supabase.co/functions/v1/register-user",
+//   {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//     body: supabaseData,
+//   }
+// );
+// return supabaseRes;
   //  else if (req.method === "GET") {
   //   } else {
   //   }
